54783877b972ce8a41f68c077a1165a384f6310e
DivviyanK18 <divviyan.ketheesraj@torontomu.ca>
2024-10-09 23:59:26 -0400

    index.js

---
+++ index.js
@@ -0,0 +1,28 @@
+document.getElementById('nameButton').addEventListener('click', function() {
+    const name = prompt("Enter your name:");
+    document.getElementById('nameDisplay').textContent = 'Hello ' + name + '!!';
+});
+
+document.getElementById('profileImage').addEventListener('click', function() {
+    if (this.style.width === '400px') {
+        this.style.width = '200px';
+        this.style.height = 'auto';
+    } else {
+        this.style.width = '400px';
+        this.style.height = 'auto';
+    }
+});
+
+const navLinks = document.querySelectorAll('nav a');
+
+navLinks.forEach(link => {
+    link.addEventListener('mouseover', function() {
+        this.style.color = 'blue';
+        this.style.fontSize = '20px';
+    });
+
+    link.addEventListener('mouseout', function() {
+        this.style.color = 'black';
+        this.style.fontSize = '18px';
+    });
+}); 
