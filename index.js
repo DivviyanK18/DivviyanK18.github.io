2745622d1c6b055b966605083bc04cf6c4b7c19d
DivviyanK18 <divviyan.ketheesraj@torontomu.ca>
2024-10-30 23:17:34 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
+// Allow the user to enter their name
 document.getElementById('nameButton').addEventListener('click', function() {
     const name = prompt("Enter your name:");
+    // show a greeting to the user once name is typed in
     document.getElementById('nameDisplay').textContent = 'Hello ' + name + '!!';
 });
-
+// check dimensions of image, reduce and expand when clicked on to the set width and height
 document.getElementById('profileImage').addEventListener('click', function() {
     if (this.style.width === '400px') {
         this.style.width = '200px';
@@ -12,9 +14,9 @@ document.getElementById('profileImage').addEventListener('click', function() {
         this.style.height = 'auto';
     }
 });
-
+// check and loop through each navigation link
 const navLinks = document.querySelectorAll('nav a');
-
+// when links are hovered over, expand size and change colour as necessary
 navLinks.forEach(link => {
     link.addEventListener('mouseover', function() {
         this.style.color = 'blue';
