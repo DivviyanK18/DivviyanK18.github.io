a0aca6a627b7f3b4d07c43fc4be50eaefb6ea3b1
DivviyanK18 <divviyan.ketheesraj@torontomu.ca>
2024-10-30 23:07:24 -0400

    Update forms.js

--- forms.js
+++ forms.js
@@ -1,17 +1,18 @@
 function saveForms() {
+// setting values up for each input field 
     const name = document.getElementById("name").value;
     const email = document.getElementById("email").value;
     const gender = document.querySelector('input[name="gender"]:checked').value;
     const feedback = document.getElementById("feedback").value;
     const favColor = document.getElementById("favColor").value;
     const subscribe = document.getElementById("subscribe").checked;
-
+// storing each value into the local storage
     localStorage.setItem("name", name);
     localStorage.setItem("email", email);
     localStorage.setItem("gender", gender);
     localStorage.setItem("feedback", feedback);
     localStorage.setItem("favColor", favColor);
     localStorage.setItem("subscribe", subscribe ? "Yes" : "No");
-
+// display an alert showing the data has been saved to the local storage
     alert("Form data saved to local storage!");
 }
